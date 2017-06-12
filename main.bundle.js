54ca9f0e4615bbfa5909b7c54a0899263c6bb3a3
FrambyChocolate <jonathan.pageaux@gmail.com>
2017-06-12 22:37:18 +0200

    Add files via upload

---
+++ main.bundle.js
@@ -0,0 +1,357 @@
+webpackJsonp([2,4],{
+
+/***/ 142:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(11)(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ 143:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(11)(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ 144:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(11)(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ 145:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(11)(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ 150:
+/***/ (function(module, exports) {
+
+module.exports = "<app-navbar></app-navbar>\n\n<!-- Contenu de la page accueil-->\n<div class=\"container main-home\">\n  <!-- barre menu-->\n  <app-menu-accueil></app-menu-accueil>\n  <div class=\"\">\n    <img class= \"img-home-accueil\" src=\"assets/images/home.jpg\"/>\n  </div>\n\n\n  <h1>ZZ SITE.</h1>\n\n  <h1>ZZ SITE.</h1>\n  <h1>ZZ SITE.</h1><h1>ZZ SITE.</h1><h1>ZZ SITE.</h1>\n  <h1>ZZ SITE.</h1>\n  <h1>ZZ SITE.</h1>\n\n\n  <h1>ZZ SITE.</h1>\n\n  <h1>ZZ SITE.</h1>\n  <h1>ZZ SITE.</h1><h1>ZZ SITE.</h1><h1>ZZ SITE.</h1><h1>ZZ SITE.</h1>\n  <h1>ZZ SITE.</h1>\n\n\n  <h1>ZZ SITE.</h1>\n\n  <h1>ZZ SITE.</h1>\n  <h1>ZZ SITE.</h1><h1>ZZ SITE.</h1><h1>ZZ SITE.</h1>\n</div>\n\n<app-footer></app-footer>\n"
+
+/***/ }),
+
+/***/ 151:
+/***/ (function(module, exports) {
+
+module.exports = "<!-- Pied de page -->\n<footer>\n  <div class=\"container\">\n    <p class=\"text-center\">&copy; Copyright 2017</p>\n  </div>\n</footer>\n"
+
+/***/ }),
+
+/***/ 152:
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"menu-accueil-custom\">\n  <div class=\"menu-accueil-custom-1\"><a class=\"link-menu-custom\" href=\"\"><p class=\"text-accueil-home\">AFROVIBES</p></a></div>\n  <div class=\"menu-accueil-custom-1\"><a class=\"link-menu-custom\" href=\"\"><p class=\"text-accueil-home\">HORS-SERIES</p></a></div>\n  <div class=\"menu-accueil-custom-1\"><a class=\"link-menu-custom\" href=\"\"><p class=\"text-accueil-home\">ACTUS</p></a></div>\n  <div class=\"menu-accueil-custom-1\"><a class=\"link-menu-custom\" href=\"\"><p class=\"text-accueil-home\">AGENDA</p></a></div>\n  <div class=\"menu-accueil-custom-1\"><a class=\"link-menu-custom\" href=\"http://www.google.fr\"><p class=\"text-accueil-home\">QUI SOMMES-NOUS</p></a></div>\n</div>\n"
+
+/***/ }),
+
+/***/ 153:
+/***/ (function(module, exports) {
+
+module.exports = "<!-- Fixed navbar ( header) -->\n<nav class=\"navbar navbar-default navbar-fixed-top\" style=\"background-color: white;\">\n  <div class=\"container\" >\n<!-- Social Network -->\n    <div class=\"navbar-custom-left\">\n      <div class=\"accueil-abonnez-vous\">\n        <p>ABONNEZ-VOUS!</p>\n      </div>\n      <div class=\"social-navbar-custom\">\n        <div class=\"btn btn-social-icon hvr-grow-shadow\">\n         <a class=\"hvr-grow-shadow\" href=\"https://www.facebook.com/AfrovibesJeSwi/\" target=\"_blank\">\n            <img src=\"assets/images/fb.png\" height=\"26px\" width=\"26px\"/>\n         </a>\n        </div>\n\n        <div class=\"btn btn-social-icon hvr-grow-shadow\">\n          <a class=\"hvr-grow-shadow\" href=\"http://www.dailymotion.com/jeswi\" target=\"_blank\">\n            <img src=\"assets/images/daily.png\" height=\"26px\" width=\"26px\"/>\n          </a>\n        </div>\n\n        <div class=\"btn btn-social-icon hvr-grow-shadow\">\n          <a class=\"hvr-grow-shadow\" href=\"https://www.youtube.com/channel/UCrlWPM7Zfi83cXWVaapt_Nw\" target=\"_blank\">\n            <img src=\"assets/images/yt.jpg\" height=\"26px\" width=\"26px\" />\n          </a>\n        </div>\n\n        <div class=\"btn btn-social-icon hvr-grow-shadow\">\n          <a class=\"hvr-grow-shadow\" href=\"https://www.instagram.com/je_swi/\" target=\"_blank\">\n            <img src=\"assets/images/instagram.jpg\" height=\"26px\" width=\"26px\"/>\n          </a>\n        </div>\n\n        <div class=\"btn btn-social-icon hvr-grow-shadow\">\n          <a class=\"\" href=\"https://www.twitter.com/je_swi/\" target=\"_blank\">\n            <img src=\"assets/images/twitter.png\" height=\"26px\" width=\"26px\"/>\n          </a>\n        </div>\n      </div>\n    </div>\n    <!-- IMAGE ACCUEIL-->\n    <div class=\"accueil-navbar-custom\">\n        <a class=\"hvr-grow-shadow\" href=\"#\">\n          <img src=\"assets/images/test.jpg\" height=\"60px\" width=\"60px\"/>\n        </a>\n    </div>\n\n  </div>\n  <div style=\"border-bottom : 2px solid #000;\" ></div>\n</nav>\n<!-- IMAGE ACCUEIL-->\n\n"
+
+/***/ }),
+
+/***/ 183:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(72);
+
+
+/***/ }),
+
+/***/ 71:
+/***/ (function(module, exports) {
+
+function webpackEmptyContext(req) {
+	throw new Error("Cannot find module '" + req + "'.");
+}
+webpackEmptyContext.keys = function() { return []; };
+webpackEmptyContext.resolve = webpackEmptyContext;
+module.exports = webpackEmptyContext;
+webpackEmptyContext.id = 71;
+
+
+/***/ }),
+
+/***/ 72:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(3);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__(80);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__(82);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__(86);
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* enableProdMode */])();
+}
+__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */]);
+//# sourceMappingURL=main.js.map
+
+/***/ }),
+
+/***/ 81:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(3);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'app works!';
+    }
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_4" /* Component */])({
+        selector: 'app-root',
+        template: __webpack_require__(150),
+        styles: [__webpack_require__(142)]
+    })
+], AppComponent);
+
+//# sourceMappingURL=app.component.js.map
+
+/***/ }),
+
+/***/ 82:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__(21);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__(3);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__(78);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_http__ = __webpack_require__(79);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app_component__ = __webpack_require__(81);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__common_navbar_navbar_component__ = __webpack_require__(85);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__common_footer_footer_component__ = __webpack_require__(83);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__common_menu_accueil_menu_accueil_component__ = __webpack_require__(84);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["b" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */],
+            __WEBPACK_IMPORTED_MODULE_5__common_navbar_navbar_component__["a" /* NavbarComponent */],
+            __WEBPACK_IMPORTED_MODULE_6__common_footer_footer_component__["a" /* FooterComponent */],
+            __WEBPACK_IMPORTED_MODULE_7__common_menu_accueil_menu_accueil_component__["a" /* MenuAccueilComponent */]
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+            __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormsModule */],
+            __WEBPACK_IMPORTED_MODULE_3__angular_http__["a" /* HttpModule */]
+        ],
+        providers: [],
+        bootstrap: [__WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]]
+    })
+], AppModule);
+
+//# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ 83:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(3);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return FooterComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var FooterComponent = (function () {
+    function FooterComponent() {
+    }
+    FooterComponent.prototype.ngOnInit = function () {
+    };
+    return FooterComponent;
+}());
+FooterComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_4" /* Component */])({
+        selector: 'app-footer',
+        template: __webpack_require__(151),
+        styles: [__webpack_require__(143)]
+    }),
+    __metadata("design:paramtypes", [])
+], FooterComponent);
+
+//# sourceMappingURL=footer.component.js.map
+
+/***/ }),
+
+/***/ 84:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(3);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return MenuAccueilComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var MenuAccueilComponent = (function () {
+    function MenuAccueilComponent() {
+    }
+    MenuAccueilComponent.prototype.ngOnInit = function () {
+    };
+    return MenuAccueilComponent;
+}());
+MenuAccueilComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_4" /* Component */])({
+        selector: 'app-menu-accueil',
+        template: __webpack_require__(152),
+        styles: [__webpack_require__(144)]
+    }),
+    __metadata("design:paramtypes", [])
+], MenuAccueilComponent);
+
+//# sourceMappingURL=menu-accueil.component.js.map
+
+/***/ }),
+
+/***/ 85:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(3);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return NavbarComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var NavbarComponent = (function () {
+    function NavbarComponent() {
+    }
+    NavbarComponent.prototype.ngOnInit = function () {
+    };
+    return NavbarComponent;
+}());
+NavbarComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_4" /* Component */])({
+        selector: 'app-navbar',
+        template: __webpack_require__(153),
+        styles: [__webpack_require__(145)]
+    }),
+    __metadata("design:paramtypes", [])
+], NavbarComponent);
+
+//# sourceMappingURL=navbar.component.js.map
+
+/***/ }),
+
+/***/ 86:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+// The file contents for the current environment will overwrite these during build.
+var environment = {
+    production: false
+};
+//# sourceMappingURL=environment.js.map
+
+/***/ })
+
+},[183]);
+//# sourceMappingURL=main.bundle.js.map
